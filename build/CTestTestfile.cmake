01204994ee9e0008d58cc1954000e19a76935d98
Michael Zeng <michael.zengzf@gmail.com>
2019-03-01 12:57:12 -0800

    latest

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/michaelz/Desktop/projects/courses/22L/k-chat-server
-# Build directory: /home/michaelz/Desktop/projects/courses/22L/k-chat-server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/home/michaelz/Desktop/projects/courses/22L/k-chat-server/bin/chess")
-set_tests_properties(test PROPERTIES  WORKING_DIRECTORY "/home/michaelz/Desktop/projects/courses/22L/k-chat-server/bin")
