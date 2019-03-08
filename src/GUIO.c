80229624f78fce1ec489a554f28bddb432b1219c
Armando <armanddr@uci.edu>
2019-03-07 18:58:59 -0800

    added text entries to register menu and a empty container function

--- src/GUIO.c
+++ src/GUIO.c
@@ -7,10 +7,11 @@ extern GtkWidget *fixed;//the widget to contain table
 extern GtkWidget *table;//the widget to contain icons
 extern GtkWidget *text_view; // widget to write text into log
 GtkWidget *username;
-GtkWidget *password;
-GtkWidget *tableU, *tableP;
+GtkWidget *password, *verifyPassword;
+GtkWidget *tableU, *tableP, *tableVP;
 GtkWidget *scrolled_win;
 GtkTextBuffer *buffer=NULL;
+GList *children, *iter;
 
 //the pixbuf to load image and resize from a .jpg or .png file
 GdkPixbuf *Login_pixbuf = NULL;
@@ -18,6 +19,16 @@ GdkPixbuf *Register_pixbuf = NULL;
 GdkPixbuf *Add_friends_pixbuf = NULL;
 char *Login_menu_path="res/Login.png";
 char *Register_menu_path="res/Register_Menu.png";
+
+void empty_container(GtkWidget *container)
+{
+    //testing the destroy stuff in a container
+    children = gtk_container_get_children(GTK_CONTAINER(container));
+    for(iter = children; iter != NULL; iter = g_list_next(iter))
+    gtk_widget_destroy(GTK_WIDGET(iter->data));
+    g_list_free(children);
+    //end test
+}
 
 gint Login_menu_callback (GtkWidget *widget, GdkEvent  *event, gpointer data)
 {
@@ -54,6 +65,7 @@ int Login_menu()
 {
     
     gdk_threads_enter();//this is important, before you call any gtk_* or g_* or gdk_* functions, call this function first
+    empty_container(layout);
     Login_pixbuf=load_pixbuf_from_file(Login_menu_path);
     Login_pixbuf=gdk_pixbuf_scale_simple(Login_pixbuf,WINDOW_WIDTH,WINDOW_HEIGHT,GDK_INTERP_BILINEAR);
     tableU=gtk_table_new (10, 10, TRUE);
@@ -101,11 +113,30 @@ gint Register_menu_callback (GtkWidget *widget, GdkEvent  *event, gpointer data)
 int Register_menu()
 {
     gdk_threads_enter();//this is important, before you call any gtk_* or g_* or gdk_* functions, call this function first
+    empty_container(layout);
     Register_pixbuf=load_pixbuf_from_file(Register_menu_path);
     Register_pixbuf=gdk_pixbuf_scale_simple(Register_pixbuf,WINDOW_WIDTH,WINDOW_HEIGHT,GDK_INTERP_BILINEAR);
-    
+    tableU=gtk_table_new (10, 10, TRUE);
+    tableP=gtk_table_new (10, 10, TRUE);
+    tableVP=gtk_table_new (10, 10, TRUE);
     image = gtk_image_new_from_pixbuf(Register_pixbuf);
+    username = gtk_entry_new();
+    password= gtk_entry_new();
+    verifyPassword= gtk_entry_new();
+    gtk_table_attach (GTK_TABLE (tableU), username, 0, 2, 0, 4,
+        GTK_EXPAND, GTK_SHRINK, 380, 168);
+    gtk_table_attach (GTK_TABLE (tableP), password, 0, 2, 0, 4,
+        GTK_EXPAND, GTK_SHRINK, 380, 219);
+    gtk_table_attach (GTK_TABLE (tableVP), verifyPassword, 0, 2, 0, 4,
+        GTK_EXPAND, GTK_SHRINK, 380, 270);
     gtk_layout_put(GTK_LAYOUT(layout), image, 0, 0);
+    gtk_container_add (GTK_CONTAINER (layout), tableU);
+    gtk_container_add (GTK_CONTAINER (layout), tableP);
+    gtk_container_add (GTK_CONTAINER (layout), tableVP);
+    gtk_entry_set_visibility(GTK_ENTRY(password), FALSE);
+    gtk_entry_set_invisible_char (GTK_ENTRY (password), '*');
+    gtk_entry_set_visibility(GTK_ENTRY(verifyPassword), FALSE);
+    gtk_entry_set_invisible_char (GTK_ENTRY (verifyPassword), '*');
 
     
     gulong handlerID=g_signal_connect(window, "button_press_event", G_CALLBACK(Register_menu_callback),NULL);
