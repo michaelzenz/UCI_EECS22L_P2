359dd80808fe10a6e9662da4530a9dc73537fd7d
klaus117 <keenanlau12@gmail.com>
2019-03-13 18:34:40 -0700

    adding friend remove friend

--- src/connection.c
+++ src/connection.c
@@ -353,9 +353,51 @@ void ChallengeUser(char *dstUser)
     free(fullBuf);
 }
 
+//Work in progress
 void AddNewFriend(char *newFriend)
 {
+    #ifdef PRINT_LOG
+    printf("adding friend %s\n",dstUser);
+    #endif
+    printf("adding friend %s\n",newFriend);
+    PackQuery pq;
+    strcpy(pq.UserName,UserName);
+    strcpy(pq.dstUser,newFriend);
+    strcpy(pq.Message,"");
+    pq.action=QUERY_ADD_FRIEND;
+    pq.portNb=PlayBetweenServerPort;
+    char str_pq[MAX_PQ_SIZE];
+    encodePackQuery(str_pq,&pq);
+    char *fullBuf=sendToServerQport(str_pq);
+    printf("%s\n",fullBuf);
+    PackAnswerQuery paq=decodeStrPAQ(fullBuf);
 
+    handlePAQ(paq);
+
+    free(fullBuf);
+}
+//work in progress
+void DeleteFriend(char *oldFriend)
+{
+    #ifdef PRINT_LOG
+    printf("adding friend %s\n",dstUser);
+    #endif
+    printf("adding friend %s\n",oldFriend);
+    PackQuery pq;
+    strcpy(pq.UserName,UserName);
+    strcpy(pq.dstUser,oldFriend);
+    strcpy(pq.Message,"");
+    pq.action=QUERY_DELETE_FRIEND;
+    pq.portNb=PlayBetweenServerPort;
+    char str_pq[MAX_PQ_SIZE];
+    encodePackQuery(str_pq,&pq);
+    char *fullBuf=sendToServerQport(str_pq);
+    printf("%s\n",fullBuf);
+    PackAnswerQuery paq=decodeStrPAQ(fullBuf);
+
+    handlePAQ(paq);
+
+    free(fullBuf);
 }
 
 void InitQueryTimeredTask()
